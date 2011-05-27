2070e66b12287db98452b4df12e68c1ff9542b4c
bskinner <bskinner@google.com>
2011-05-27 11:20:30 -0700

    added basic initial support for uploading wikipedia articles

--- src/com/google/dotorg/translation_workflow/io/TranslatorToolkitUtil.java
+++ src/com/google/dotorg/translation_workflow/io/TranslatorToolkitUtil.java
@@ -217,7 +217,9 @@ public class TranslatorToolkitUtil {
         FakeFileSource fakeFileSource = new FakeFileSource(htmlContent);
         entry.setMediaSource(fakeFileSource);
       } else {
-        DocumentSource docSource = new DocumentSource(DocumentSource.Type.HTML, articleUrl);
+        DocumentSource.Type type = 
+            isWikipediaArticle(articleUrl) ? DocumentSource.Type.WIKI : DocumentSource.Type.HTML;
+        DocumentSource docSource = new DocumentSource(type, articleUrl);
         entry.setDocumentSource(docSource);
       }
       
@@ -228,6 +230,46 @@ public class TranslatorToolkitUtil {
       return service.insert(feedUrl, entry);     
     }
   }
+  
+  // returns true if the given URL points to a Wikipedia article
+  private boolean isWikipediaArticle(String articleUrl) {
+    articleUrl.startsWith("http://en.wikipedia.org/");
+    return true;
+  }
+  
+  /*
+  private DocumentEntry uploadWikipediaDocument(
+      String sourceLangCode, String targetLangCode, String title, String articleUrl,
+      String htmlContent) 
+      throws IOException, ServiceException {
+    
+    if (service == null) {
+      return null;
+    } else {
+      DocumentEntry entry = new DocumentEntry();
+      entry.setSourceLanguage(new SourceLanguage(sourceLangCode));
+      entry.setTargetLanguage(new TargetLanguage(targetLangCode));
+      entry.setTitle(new PlainTextConstruct(title));
+
+      DocumentSource docSource = new DocumentSource(DocumentSource.Type.WIKI, row.getArticleURL());
+      entry.setDocumentSource(docSource);
+
+      GlossariesElement gl = new GlossariesElement();
+      String glHref = FeedUris.getGlossaryFeedUrl(healthGlossaryID).toString();
+
+      Link glLink = new Link();
+      glLink.setHref(glHref);
+
+      gl.addLink(glLink);
+      entry.setGlossary(gl);      
+      setGlossary(entry, glossaryId);
+      setTranslationMemory(entry, translationMemoryId);
+      
+      URL feedUrl = new URL(DOC_FEED_URL);
+      return service.insert(feedUrl, entry);
+    }
+  }
+  */
   
   private void setGlossary(DocumentEntry entry, String glossaryId) {
     if (glossaryId != null && !glossaryId.isEmpty()) {
