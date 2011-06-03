fdcc17b5265509e453c5279b03b7065f9654239f
kparlante <kparlante@gmail.com>
2011-06-03 10:42:01 -0700

    Step 1 of the fix for Bug 5, set up web.xml info for JSPs to remove .jsp extension

---
+++ src/com/google/dotorg/translation_workflow/servlet/RefreshProgressServlet.java
@@ -0,0 +1,41 @@
+package com.google.dotorg.translation_workflow.servlet;
+
+import com.google.appengine.api.users.User;
+import com.google.appengine.api.users.UserService;
+import com.google.appengine.api.users.UserServiceFactory;
+import com.google.dotorg.translation_workflow.model.Cloud;
+import com.google.dotorg.translation_workflow.model.Project;
+
+
+import java.io.IOException;
+import java.util.logging.Logger;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+public class RefreshProgressServlet extends HttpServlet {
+    private static final Logger logger = Logger.getLogger(ClaimServlet.class.getName());
+    
+    @Override
+    public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
+      UserService userService = UserServiceFactory.getUserService();
+      User user = userService.getCurrentUser();
+
+      // read the input parameters from the client and validate them all before using the values
+      String rawProjectId = request.getParameter("projectId");
+      int projectId = Integer.parseInt(rawProjectId);
+      
+      Cloud cloud = Cloud.open();
+      Project project = cloud.getProjectById(projectId);
+      cloud.refreshTranslationStatusFromToolkit(user, project);
+      cloud.close();
+      
+      logger.info("Update translation status for user:" + user.getNickname() + " and project: " + project.getName());
+      
+      response.sendRedirect(
+          "/page/my_translations.jsp?project=" + projectId);
+    }
+
+}
